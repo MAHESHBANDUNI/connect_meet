606f451995dcaa5820faa0dd3acd2e0453bffca2
Mahesh Banduni <BL0159@bliss.local>
2026-01-12 18:24:00 +0530

    Updated .gitignore file

--- server/drizzle/0001_cultured_sabretooth.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "google_id" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "auth_provider" text DEFAULT 'local' NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "is_email_verified" text DEFAULT 'false' NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_google_id_unique" UNIQUE("google_id");
